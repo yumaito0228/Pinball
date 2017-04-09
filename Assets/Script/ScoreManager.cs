a16c911922802a9070f000ecfa6884fbc7b500a0
yumaito <yuma19950228@icloud.com>
2017-04-09 16:17:10 +0700

    Pinball
    課題：得点を表示
    発展課題：スマートフォンでも動かせるようにマルチタッチに対応

---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public Text scoretext;
+	public static int scorevalue;
+	private GameObject score;
+
+
+	// Use this for initialization
+	void Start () {
+		scorevalue = 0;
+		this.score = GameObject.Find("Score");
+		this.score.GetComponent<Text> ().text = "Score: " + scorevalue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter (Collision other){
+		if (other.gameObject.tag == "SmallStarTag") {
+			scorevalue += 1000;
+			this.score.GetComponent<Text> ().text = "Score: " + scorevalue;
+		}
+		if (other.gameObject.tag == "SmallCloudTag") {
+			scorevalue += 2000;
+			this.score.GetComponent<Text> ().text = "Score: " + scorevalue;
+		}
+		if (other.gameObject.tag == "LargeStarTag") {
+			scorevalue += 1500;
+			this.score.GetComponent<Text> ().text = "Score: " + scorevalue;
+		}
+		if (other.gameObject.tag == "LargeCloudTag") {
+			scorevalue += 2500;
+			this.score.GetComponent<Text> ().text = "Score: " + scorevalue;
+		}
+
+	}
+}
---
+++ Assets/Script/TouchController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+
+	public GameObject leftup;
+	public GameObject leftdown;
+	public GameObject rightup;
+	public GameObject rightdown;
+
+	GameObject leftfripper;
+	GameObject rightfripper;
+
+	private Rect rect;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 point_leftup = leftup.transform.position;
+		Vector3 point_leftdown = leftdown.transform.position;
+		Vector3 point_rightup = leftup.transform.position;
+		rect = new Rect(point_leftup.x,point_leftup.y,Screen.height,Screen.width);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		int count = Input.touchCount;
+
+		for (int i = 0; i < count; i++) {
+			Touch touch = Input.GetTouch (i);
+			Vector2 Vec = new Vector2 (touch.position.x, Screen.height - touch.position.y);
+			if (touch.phase == TouchPhase.Began) {
+				//右側
+				if (Vec.x <= rect.width / 2 && Vec.y >= rect.yMin) {
+					FripperController leftfc = leftfripper.GetComponent<FripperController> ();
+					leftfc.SetAngle (leftfc.flickAngle);
+				}
+				//左側
+				if (Vec.x >= rect.width / 2 && Vec.y >= rect.yMin) {
+					FripperController rightfc = rightfripper.GetComponent<FripperController> ();
+					rightfc.SetAngle (rightfc.flickAngle);
+				}
+				if (Input.touchCount > 1) {
+				}
+			}
+			if (touch.phase == TouchPhase.Ended) {
+				FripperController leftfc = leftfripper.GetComponent<FripperController> ();
+				leftfc.SetAngle (leftfc.defaultAngle);
+				FripperController rightfc = rightfripper.GetComponent<FripperController> ();
+				rightfc.SetAngle (rightfc.defaultAngle);
+			}
+		}
+	}
+}
